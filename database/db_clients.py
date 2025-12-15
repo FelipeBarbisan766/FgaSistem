cd1706ebe7d31a4a3368da4d919b601bd2ef73a4
FelipeBarbisan766 <felipebarbisan76@gmail.com>
2025-12-14 21:24:55 -0300

    create add client

--- database/db_clients.py
+++ database/db_clients.py
@@ -4,3 +4,10 @@ def search_clients():
     cursor = conn.cursor()
     cursor.execute("SELECT clients.id, clients.name,clients.address, clients.email, clients.phone FROM clients;")
     return cursor.fetchall()
+
+def insert_client(name, address, phone, email):
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO clients (name, address, phone, email) VALUES (?, ?, ?, ?);", (name, address, phone, email))
+    conn.commit()
+    return True

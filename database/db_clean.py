100f628c87dd020a55347b5140971e5e4ec583c4
FelipeBarbisan766 <felipebarbisan76@gmail.com>
2025-12-22 23:05:19 -0300

    remake the clean table

--- database/db_clean.py
+++ database/db_clean.py
@@ -1,14 +1,54 @@
 from database.db_manager import get_connection
 
-def search_cleans():
+def search_clean_paginated(limit: int, offset: int):
     conn = get_connection()
     cursor = conn.cursor()
-    cursor.execute("SELECT cleans.id, cleans.date, cleans.price, cleans.quantity, cleans.unitPrice, cleans.status, clients.name FROM cleans LEFT JOIN clients ON cleans.clientId = clients.id;")
+
+    cursor.execute(
+        """
+        SELECT cleans.id, cleans.date, cleans.price, cleans.quantity, cleans.unitPrice, cleans.status, clients.name
+        FROM cleans
+        LEFT JOIN clients ON cleans.clientId = clients.id
+        ORDER BY cleans.id
+        LIMIT ? OFFSET ?;
+        """,
+        (limit, offset),
+    )
+    return cursor.fetchall()
+
+def count_clean():
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT COUNT(*) FROM cleans;")
+    (total,) = cursor.fetchone()
+    return total
+
+def search_clean():
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute(
+        """
+        SELECT cleans.id, cleans.date, cleans.price, cleans.quantity, cleans.unitPrice, cleans.status, clients.name
+        FROM cleans
+        LEFT JOIN clients ON cleans.clientId = clients.id
+        ORDER BY cleans.id;
+        """
+    )
     return cursor.fetchall()
 
 def insert_clean(date, price, quantity, unitPrice, status, clientID):
     conn = get_connection()
     cursor = conn.cursor()
-    cursor.execute("INSERT INTO cleans (date, price, quantity, unitPrice, status, clientId) VALUES (?, ?, ?, ?, ?, ?);", (date, price, quantity, unitPrice, status, clientID))
+    cursor.execute(
+        "INSERT INTO cleans (date, price, quantity, unitPrice, status, clientId) VALUES (?, ?, ?, ?, ?, ?);",
+        (date, price, quantity, unitPrice, status, clientID),
+    )
+    conn.commit()
+    return True
+
+def remove_clean(clean_id):
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM cleans WHERE id = ?;", (clean_id,))
     conn.commit()
     return True

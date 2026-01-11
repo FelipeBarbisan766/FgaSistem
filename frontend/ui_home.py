747235e0c23da55d905613336b9316a6e1935eaf
FelipeBarbisan766 <felipebarbisan76@gmail.com>
2026-01-10 23:50:46 -0300

    commit

--- frontend/ui_home.py
+++ frontend/ui_home.py
@@ -4,6 +4,6 @@ class HomeFrame(tk.Frame):
     def __init__(self, master):
         super().__init__(master)
         tk.Label(self, text="Bem-vindo à tela principal!").pack(pady=20)
-        tk.Button(self, text="Limpezas", command=lambda: master.show_frame("CleanFrame")).pack(pady=10)
-        tk.Button(self, text="Clientes", command=lambda: master.show_frame("ClientFrame")).pack(pady=10)
+        tk.Button(self, text="Tela de Limpezas", command=lambda: master.show_frame("CleanFrame")).pack(pady=10)
+        tk.Button(self, text="Tela de Clientes", command=lambda: master.show_frame("ClientFrame")).pack(pady=10)
         tk.Button(self, text="Sair", command=lambda: master.show_frame("LoginFrame")).pack(pady=10)
